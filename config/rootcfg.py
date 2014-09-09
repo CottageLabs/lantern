2eaec0b5c8d391b7651245b3246224d3176e0923
Richard Jones <richard@cottagelabs.com>
2014-09-09 14:37:49 +0100

    first basic working version with new approach

---
+++ config/rootcfg.py
@@ -0,0 +1,9 @@
+# absolute paths, or relative paths from root directory, to the desired config files
+CONFIG_FILES = [
+    "portentious/portality/config/webapp.py"
+]
+
+# absolute paths, or relative paths from root directory, to the template directories
+TEMPLATE_PATHS = [
+    "portentious/portality/templates"
+]
