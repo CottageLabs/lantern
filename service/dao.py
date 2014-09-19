c8870da355ca99103bcbef712d0367463d3c068f
Richard Jones <richard@cottagelabs.com>
2014-09-19 14:48:03 +0100

    upgrade esprit and porentious implementations and also template dao

--- service/dao.py
+++ service/dao.py
@@ -1,6 +1,6 @@
 import esprit
 from portality.core import app
 
-class MyDAO(object):
+class MyDAO(esprit.dao.DomainObject):
     __type__ = 'index'
     __conn__ = esprit.raw.Connection(app.config.get('ELASTIC_SEARCH_HOST'), app.config.get('ELASTIC_SEARCH_INDEX'))
